a621cb9e456c233951718a5e27d04ae4d4713653
fatima ezzahraa el <fatimzahraelmadini@gmail.com>
2022-07-08 15:07:47 +0200

    Cleaning the function from console.log

--- src/App.js
+++ src/App.js
@@ -32,33 +32,19 @@ function App() {
    
     
     for (let i =0; i < chunkArray.length; i ++){
-      console.log("the element is ")
-      console.log(chunkArray[i])
-      console.log(`Its place is: ${i}`)
+   
       // If the element is an opening 
 
       if(chunkArray[i] ==="(" || chunkArray[i] ==="[" || chunkArray[i] ==="{" || chunkArray[i] ==="<")  {
      
-        console.log("------------------------------")
-
-        // If the closures array is not empty and not having the same amount of the openings array it is a mistake
-        // if (previousClosuresArray.length>0 && previousClosuresArray.length!== previousOpensArray.length){
-        //   console.log(`the index is: ${i} and previousClosuresArray length is : ${previousClosuresArray}`)
-        //   break;
-
-        // }
-
         // We push it to the array of oppenings
    
         previousOpensArray.push({element:chunkArray[i], status:true})
-        console.log("the array of previousOpensArray is:")
-        console.log(previousOpensArray)
+    
+
         // We put the last opening againg to the last one in openings array
         lastOpenedplace=1
-       
-        console.log("lastOpenedplace is:")
-        console.log(lastOpenedplace)
-        console.log("------------------------------")
+     
        
       }
       // If the element is a closing 
@@ -66,10 +52,9 @@ function App() {
       else if (chunkArray[i] ===")" || chunkArray[i] ==="]" || chunkArray[i] ==="}" || chunkArray[i] ===">")
       {
         // The element should be matching the right closing of the last opening in the 'previousOpensArray'
-        console.log("------------------------------")
+   
         lastOpening = previousOpensArray[previousOpensArray.length-lastOpenedplace].element
-        console.log("lastOpening is:")
-        console.log(lastOpening)
+      
 
         switch (lastOpening) {
           case "(":
@@ -101,35 +86,24 @@ function App() {
         else if (chunkArray[i] === closingOfLastOpening )
         {
           // If it is matching we move to the next one and match it with the opening before the one that was the last opening
-            // previousClosuresArray.push(chunkArray[i] )
-            // console.log("the array of previousClosuresArray is:")
-            // console.log(previousClosuresArray)
           
             previousOpensArray[previousOpensArray.length-lastOpenedplace].status=false
-            console.log("it is matching with the last opened one so we make is false this one:")
-            console.log(previousOpensArray[previousOpensArray.length-lastOpenedplace])
-           
-
+          
             let c = previousOpensArray.length
             lastOpenedplace= 1
-            console.log(`c equals: ${c}`)
-
-            console.log(`lastOpenedplace first is: ${lastOpenedplace}`)
-
+         
             while(c!==0 && 
               
               previousOpensArray[c-1].status === false){
-                console.log(" previousOpensArray[c-1] is:")
-                console.log(previousOpensArray[c-1])
-
+              
               lastOpenedplace++
               c=c-1
             }
-            console.log(`lastOpenedplace after is: ${lastOpenedplace}`)
+        
 
           
         }
-        console.log("------------------------------")
+  
       }
 
 
